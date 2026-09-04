f90e3a847165bfd0408e767507b7fbc800814649
sv1tluy <saimonsaymon48@gmail.com>
2026-09-04 20:54:52 +0100

    Add files via upload

--- setup_hunter.py
+++ setup_hunter.py
@@ -1,3 +1,6 @@
+from flask import Flask
+app = Flask(__name__)
+
 import os
 import time
 import requests
@@ -195,3 +198,12 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+@app.route('/')
+def health_check():
+return "OK", 200
+
+if __name__ == '__main__':
+import os
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
